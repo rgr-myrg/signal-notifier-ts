f1ff337405bb6cc5852b7c6067fda3ce8bc605c8
Roger <rpmyrg@gmail.com>
2018-09-16 14:24:21 -0400

    Added Signal export

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 export { INotifier } from "./INotifier";
 export { Notification } from "./Notification";
 export { Notifier } from "./Notifier";
+export { Signal } from "signal-ts";
--- dist/index.js
+++ dist/index.js
@@ -4,4 +4,6 @@ var Notification_1 = require("./Notification");
 exports.Notification = Notification_1.Notification;
 var Notifier_1 = require("./Notifier");
 exports.Notifier = Notifier_1.Notifier;
+var signal_ts_1 = require("signal-ts");
+exports.Signal = signal_ts_1.Signal;
 //# sourceMappingURL=index.js.map
